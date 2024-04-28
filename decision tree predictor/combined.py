7927879914a804f78f11aa3334a73c2465038af6
Shreeansh04 <92516000+Shreeansh04@users.noreply.github.com>
2024-04-28 11:32:02 +0530

    combined updated (probabillity and csv)

--- decision tree predictor/combined.py
+++ decision tree predictor/combined.py
@@ -6,19 +6,14 @@ from headpose import estimate_head_pose
 from sound_detection import detect_sound
 import pickle  # Import the pickle module to load the trained model
 from sklearn.preprocessing import LabelEncoder
+from Probability_test import run
 
 # Load the trained decision tree classifier
 with open('decision_tree_model.pkl', 'rb') as f:
     decision_tree_classifier = pickle.load(f)
 
 # Initialize the DataFrame
-df = pd.DataFrame(columns=['Timestamp', 'X_AXIS_CHEAT', 'Y_AXIS_CHEAT', 'Audio_Cheat','Emotion'])
-
-# Initialize the plot
-
-
-# Initialize an empty list to store dB values
-
+df = pd.DataFrame(columns=['Timestamp', 'X_AXIS_CHEAT', 'Y_AXIS_CHEAT', 'Audio_Cheat', 'Emotion', 'Cheating?'])
 
 # Open the webcam
 cap = cv2.VideoCapture(0)
@@ -53,23 +48,23 @@ while keep_running:
                    'Emotion': max_emotion
                    }
         df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
-        df.to_csv('head_pose_emotion_detection.csv', index=False)
 
-        # Use the decision tree classifier to predict cheating
-        le = LabelEncoder()
-        X = df
-        print(X)
-        X = X.drop(['Timestamp'], axis=1)
+    # Use the decision tree classifier to predict cheating for the current timestamp
+    le = LabelEncoder()
+    X = df.loc[df['Timestamp'] == timestamp].drop(['Timestamp', 'Cheating?'], axis=1)
+    if not X.empty:
         X['Emotion'] = le.fit_transform(X['Emotion'])
-        cheating_prediction = decision_tree_classifier.predict(X)
-        print(f"CHEATING : {cheating_prediction}")
-        print("\n")
+        cheating_predictions = decision_tree_classifier.predict(X)
+
+        # Update the 'Cheating?' column for the current timestamp
+        df.loc[df['Timestamp'] == timestamp, 'Cheating?'] = cheating_predictions
 
     # Convert the image back to BGR for OpenCV
     processed_image = cv2.cvtColor(processed_image, cv2.COLOR_RGB2BGR)
 
     cv2.imshow('Head Pose and Emotion Detection', processed_image)
-
+    df.to_csv('head_pose_emotion_detection.csv', index=False)
+    run()
     # Check for the escape key press
     key = cv2.waitKey(5) & 0xFF
     if key == 27:  # Escape key
@@ -77,10 +72,7 @@ while keep_running:
 
 # Clean up resources
 cap.release()
-
 cv2.destroyAllWindows()
 
 # Save the DataFrame to a CSV file
 df.to_csv('head_pose_emotion_detection.csv', index=False)
-
-
